f8a1286d53d4f3d3816a91955fd74bd50b50cf52
Raidon <ramiahraidon@gmail.com>
2023-10-27 11:03:30 +1300

    Add getAllOrder fn

--- server/routes/customers.js
+++ server/routes/customers.js
@@ -3,15 +3,6 @@ import * as db from '../db/db.js'
 
 const router = express.Router()
 
-router.get('/', async (req, res) => {
-  try {
-    const users = await db.getUsers()
-    res.render('index', { users: users })
-  } catch (err) {
-    res.status(500).send('DATABASE ERROR: ' + err.message)
-  }
-})
-
 //function to list all customers
 
 //TODO: check function names from db
